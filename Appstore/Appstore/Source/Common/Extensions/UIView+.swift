4fed682f3cfdd980c039eea856ebd8ad3a1ac3b7
wody-d <maxwodyd98@gmail.com>
2022-06-05 01:11:17 +0900

    feat: ContentView 모서리, 그림자 추가

--- Appstore/Appstore/Source/Common/Extensions/UIView+.swift
+++ Appstore/Appstore/Source/Common/Extensions/UIView+.swift
@@ -13,4 +13,24 @@ extension UIView {
             addSubview(view)
         }
     }
+    
+    func addRadius(_ radius: CGFloat) {
+        self.layer.cornerRadius = 12
+    }
+    
+    func setBackgroundColor(_ color: UIColor) {
+        self.backgroundColor = color
+    }
+    
+    func addShadow(
+        color: UIColor,
+        opacity: Float,
+        offset: CGSize,
+        radius: CGFloat
+    ) {
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOpacity = opacity
+        self.layer.shadowOffset = offset
+        self.layer.shadowRadius = radius
+    }
 }
