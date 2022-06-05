8115b56765eb4a91cce93ae621fb913263898630
wody-d <maxwodyd98@gmail.com>
2022-06-05 14:31:08 +0900

    feat: ContentView 내부 UI 구성

--- Appstore/Appstore/Source/Views/Today/ContentCollectionViewCell.swift
+++ Appstore/Appstore/Source/Views/Today/ContentCollectionViewCell.swift
@@ -8,15 +8,25 @@
 import UIKit
 
 class ContentCollectionViewCell: BaseCollectionViewCell {
+    @IBOutlet weak var shadowView: UIView!
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet var appViews: [UIView]!
     
     override func attribute() {
         super.attribute()
-        containerView.addRadius(12) 
+        containerView.addRadius(12)
+        containerView.layer.masksToBounds = true
         containerView.setBackgroundColor(.red)
-        containerView.addShadow(color: .black.withAlphaComponent(0.4),
+        shadowView.addRadius(12)
+        shadowView.addShadow(color: .black.withAlphaComponent(0.4),
                                 opacity: 0.4,
                                 offset: .init(width: 0, height: 3),
                                 radius: 10)
+        appViews.forEach {
+            $0.addRadius(15)
+            $0.addBorder(.gray, 0.5)
+        }
+        
     }
 }
