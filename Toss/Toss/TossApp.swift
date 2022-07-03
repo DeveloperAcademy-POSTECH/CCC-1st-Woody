87fa2db4c4d986bd104a26aa769db6c9d57c264f
wody-d <maxwodyd98@gmail.com>
2022-07-03 22:33:07 +0900

    :tada: Init new toss project

---
+++ Toss/Toss/TossApp.swift
@@ -0,0 +1,17 @@
+//
+//  TossApp.swift
+//  Toss
+//
+//  Created by Woody on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct TossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
