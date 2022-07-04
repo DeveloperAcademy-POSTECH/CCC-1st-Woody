c44e5f6087f1469566c55cd2e974ba648d59b7fe
wody-d <maxwodyd98@gmail.com>
2022-07-05 00:18:15 +0900

    :recycle: HomeHeader 코드 개선

--- Toss/Toss/Views/HomeHeader.swift
+++ Toss/Toss/Views/HomeHeader.swift
@@ -11,48 +11,65 @@ struct HomeHeader: View {
     
     var body: some View {
         HStack {
-            HStack(alignment: .center, spacing: 8) {
-                Image(systemName: "dollarsign.circle.fill")
-                    .resizable()
-                    .frame(width: 24, height: 24, alignment: .center)
-                Text("toss")
-                    .font(.system(size: 30, weight: .bold, design: .rounded))
-                    .padding(.bottom, 3)
-                // MARK: 아이콘 보정때문에 텍스트와 센터를 일치시키기 위하여 패딩을 주었어요.
-                // MARK: 참고 자료 - 이슈 #16
-            }
-            .foregroundColor(Pallete.basicGray.color)
+            logoMark()
             
             Spacer()
             
             HStack(alignment: .center, spacing: 24) {
-                Button {
-                    print("Tap Message")
-                } label: {
-                    Image(systemName: "message.fill")
-                        .resizable()
-                        .frame(width: 24, height: 24, alignment: .center)
-                        
-                }
-
-                ZStack {
-                    Button {
-                        print("Tap Alarm")
-                    } label: {
-                        Image(systemName: "bell.fill")
-                            .resizable()
-                            .frame(width: 24, height: 24, alignment: .center)
-                    }
-                    Pallete.red.color
-                        .frame(width: 6, height: 6, alignment: .center)
-                        .cornerRadius(3)
-                        .offset(x: 12, y: -12)
-                }
+                messageButton()
+                alarmButton()
             }
-            .foregroundColor(Pallete.basicGray.color)
         }
         .padding(10)
     }
+    
+    @ViewBuilder
+    private func logoMark() -> some View {
+        HStack(alignment: .center, spacing: 8) {
+            Image(systemName: "dollarsign.circle.fill")
+                .resizable()
+                .frame(width: 24, height: 24, alignment: .center)
+            Text("toss")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .padding(.bottom, 3)
+            // MARK: 아이콘 보정때문에 텍스트와 센터를 일치시키기 위하여 패딩을 주었어요.
+            // MARK: 참고 자료 - 이슈 #16
+        }
+        .foregroundColor(Pallete.basicGray.color)
+    }
+    
+    @ViewBuilder
+    private func messageButton() -> some View {
+        Button {
+            print("Tap Message")
+        } label: {
+            Image(systemName: "message.fill")
+                .resizable()
+                .frame(width: 24, height: 24, alignment: .center)
+                
+        }
+        .foregroundColor(Pallete.basicGray.color)
+    }
+    
+    @ViewBuilder
+    private func alarmButton() -> some View {
+        ZStack {
+            Button {
+                print("Tap Alarm")
+            } label: {
+                Image(systemName: "bell.fill")
+                    .resizable()
+                    .frame(width: 24, height: 24, alignment: .center)
+            }
+            
+            Pallete.red.color
+                .frame(width: 6, height: 6, alignment: .center)
+                .cornerRadius(3)
+                .offset(x: 12, y: -12)
+        }
+        .foregroundColor(Pallete.basicGray.color)
+    }
+    
 }
 
 struct HomeHeader_Previews: PreviewProvider {
