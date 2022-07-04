ae85fd1317f64761183fdf653b498dc920948e8b
wody-d <maxwodyd98@gmail.com>
2022-07-05 01:33:53 +0900

    :recycle: ContentView 코드 수정

--- Toss/Toss/ContentView.swift
+++ Toss/Toss/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedTab: Tab
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        CustomTabView(selectedTab: $selectedTab)
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(selectedTab: .home)
     }
 }
