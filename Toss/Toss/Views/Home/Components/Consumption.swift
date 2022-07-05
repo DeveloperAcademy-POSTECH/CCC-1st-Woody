06dd666c64e8fa2c399f408b0923fa52efbac525
wody-d <maxwodyd98@gmail.com>
2022-07-05 13:03:22 +0900

    :recycle: Home 코드 개선

--- Toss/Toss/Views/Home/Components/Consumption.swift
+++ Toss/Toss/Views/Home/Components/Consumption.swift
@@ -19,36 +19,49 @@ struct Consumption: View {
         } label: {
             
             HStack(spacing: 16) {
-                Image("card")
-                    .resizable()
-                    .frame(width: 42, height: 42, alignment: .center)
-                VStack(alignment: .leading, spacing: 6) {
-                    Text("이번달 쓴 금액")
-                        .foregroundColor(Pallete.subGray.color)
-                        .font(.system(size: 14, weight: .medium, design: .rounded))
-                    Text("1,436,413 원")
-                        .foregroundColor(Pallete.black.color)
-                        .font(.system(size: 20, weight: .semibold, design: .rounded))
-                }
-                
+                image()
+                text()
                 Spacer()
-                Button {
-                    print("송금")
-                } label: {
-                    Text("새 내역 10건+")
-                        .foregroundColor(.blue)
-                        .font(.system(size: 14, weight: .semibold, design: .rounded))
-                        .padding([.leading, .trailing], 10)
-                        .padding([.top, .bottom], 8)
-                        .background(.blue.opacity(0.1))
-                        .cornerRadius(6)
-                }
-                
+                button()
             }
             .padding(20)
             .background(Pallete.white.color)
             .cornerRadius(radius, corners: .bottomLeft)
             .cornerRadius(radius, corners: .bottomRight)
+        }
+    }
+    
+    @ViewBuilder
+    private func image() -> some View {
+        Image("card")
+            .resizable()
+            .frame(width: 42, height: 42, alignment: .center)
+    }
+    
+    @ViewBuilder
+    private func text() -> some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Text("이번달 쓴 금액")
+                .foregroundColor(Pallete.subGray.color)
+                .font(.system(size: 14, weight: .medium, design: .rounded))
+            Text("1,436,413 원")
+                .foregroundColor(Pallete.black.color)
+                .font(.system(size: 20, weight: .semibold, design: .rounded))
+        }
+    }
+    
+    @ViewBuilder
+    private func button() -> some View {
+        Button {
+            print("송금")
+        } label: {
+            Text("새 내역 10건+")
+                .foregroundColor(.blue)
+                .font(.system(size: 14, weight: .semibold, design: .rounded))
+                .padding([.leading, .trailing], 10)
+                .padding([.top, .bottom], 8)
+                .background(.blue.opacity(0.1))
+                .cornerRadius(6)
         }
     }
 }
--- Toss/Toss/Views/Home/Components/HomeAssets.swift
+++ Toss/Toss/Views/Home/Components/HomeAssets.swift
@@ -15,44 +15,55 @@ struct HomeAssets: View {
     }
     var body: some View {
         Button {
-            print("Tapp 토스뱅크")
+            print("Tap 토스뱅크")
         } label: {
-            
             HStack(spacing: 16) {
-                Image("toss_logo")
-                    .resizable()
-                    .frame(width: 42, height: 42, alignment: .center)
-                    .cornerRadius(21)
-                    .font(.system(size: 22, weight: .bold, design: .monospaced))
-                VStack(alignment: .leading, spacing: 6) {
-                    Text("토스뱅크 통장")
-                        .foregroundColor(Pallete.subGray.color)
-                        .font(.system(size: 14, weight: .medium, design: .rounded))
-                    Text("436,089 원")
-                        .foregroundColor(Pallete.black.color)
-                        .font(.system(size: 20, weight: .semibold, design: .rounded))
-                }
-
+                imageIcon()
+                bankText()
                 Spacer()
-                Button {
-                    print("송금")
-                } label: {
-                    Text("송금")
-                        .foregroundColor(Pallete.subGray2.color)
-                        .font(.system(size: 14, weight: .semibold, design: .rounded))
-                        .padding([.leading, .trailing], 16)
-                        .padding([.top, .bottom], 10)
-                        .background(Pallete.backgroundGray.color)
-                        .cornerRadius(6)
-                }
-
+                sendButton()
             }
             .padding(20)
             .background(Pallete.white.color)
             .cornerRadius(radius, corners: .bottomLeft)
             .cornerRadius(radius, corners: .bottomRight)
         }
-
+    }
+    
+    @ViewBuilder
+    private func imageIcon() -> some View {
+        Image("toss_logo")
+            .resizable()
+            .frame(width: 42, height: 42, alignment: .center)
+            .cornerRadius(21)
+            .font(.system(size: 22, weight: .bold, design: .monospaced))
+    }
+    
+    @ViewBuilder
+    private func bankText() -> some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Text("토스뱅크 통장")
+                .foregroundColor(Pallete.subGray.color)
+                .font(.system(size: 14, weight: .medium, design: .rounded))
+            Text("436,089 원")
+                .foregroundColor(Pallete.black.color)
+                .font(.system(size: 20, weight: .semibold, design: .rounded))
+        }
+    }
+    
+    @ViewBuilder
+    private func sendButton() -> some View {
+        Button {
+            print("송금")
+        } label: {
+            Text("송금")
+                .foregroundColor(Pallete.subGray2.color)
+                .font(.system(size: 14, weight: .semibold, design: .rounded))
+                .padding([.leading, .trailing], 16)
+                .padding([.top, .bottom], 10)
+                .background(Pallete.backgroundGray.color)
+                .cornerRadius(6)
+        }
     }
 }
 
