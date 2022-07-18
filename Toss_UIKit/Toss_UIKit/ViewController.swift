01dfb46c003530b5b56cc74ad45eb12cab5300c5
wody-d <maxwodyd98@gmail.com>
2022-07-19 00:30:49 +0900

    🎉 프로젝트 이닛

---
+++ Toss_UIKit/Toss_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Toss_UIKit
+//
+//  Created by Woody on 2022/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
